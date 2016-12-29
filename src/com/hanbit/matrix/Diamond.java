d56befceb00098ae69e82690732441e161a75e17
rufruv <rufruv@gmail.com>
2016-12-29 18:59:02 +0900

    Add Comment

--- src/com/hanbit/matrix/Diamond.java
+++ src/com/hanbit/matrix/Diamond.java
@@ -8,20 +8,24 @@ package com.hanbit.matrix;
  * */
 public class Diamond {
     public static void main(String[] args) {
-        int len=5, col= len, row= len, val=0, start=0, end=0; //배열에 저장할 변수
+        int len=5, col=len, row=len, val=0, start=0, end=0; //배열에 저장할 변수
         int[][] arr = new int[col][row];
         start=3; //열의 시작 위치
         end=3; //열의 끝 위치
         int i = 0;
         int j = 0;
+       
         
-        for(i=0; i<arr.length; i++){
-        	System.out.println("");
-        	for(j=0; j<arr[i].length; j++){
-        		System.out.printf("[%d,%d]", i,j);
-        	}
-        }
-
-
-    }
+        for (i = 0; i < arr.length; i++) {
+			System.out.println(" ");
+			for (j = 0; j < arr[i].length; j++) {
+				switch(i){
+				case 0 : 
+					System.out.print((j==2)?start+" ":val+" ");
+				}
+				//arr[i][j]=k+=5 ;
+				//System.out.printf("%d\t",arr[i][j]);
+			}
+		}
+	}
 }
--- src/com/hanbit/matrix/MatrixDiff5.java
+++ src/com/hanbit/matrix/MatrixDiff5.java
@@ -1,10 +1,10 @@
 package com.hanbit.matrix;
 
 /*[0,0][0,1][0,2][0,3][0,4] 
-[1,0][1,1][1,2][1,3][1,4] 
-[2,0][2,1][2,2][2,3][2,4] 
-[3,0][3,1][3,2][3,3][3,4] 
-[4,0][4,1][4,2][4,3][4,4]*/
+  [1,0][1,1][1,2][1,3][1,4] 
+  [2,0][2,1][2,2][2,3][2,4] 
+  [3,0][3,1][3,2][3,3][3,4] 
+  [4,0][4,1][4,2][4,3][4,4]*/
 public class MatrixDiff5 {
 	public static void main(String[] args) {
 		int[][] mtx = new int[5][5];
@@ -14,7 +14,7 @@ public class MatrixDiff5 {
 		for (int i = 0; i < mtx.length; i++) {
 			System.out.println(" ");
 			for (int j = 0; j < mtx[i].length; j++) {
-				mtx[i][j]=k+=5;
+				mtx[i][j]=k+=5 ;
 				System.out.printf("%d\t",mtx[i][j]);
 			}
 		}

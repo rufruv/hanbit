2a26cc6e0eb85cc40b08f0968b28fdc01d8351f1
rufruv <rufruv@gmail.com>
2016-12-30 17:23:54 +0900

    Add Comment

--- src/com/hanbit/matrix/Zigzag.java
+++ src/com/hanbit/matrix/Zigzag.java
@@ -1,4 +1,5 @@
 package com.hanbit.matrix;
+
 /*
   1   2   3   4   5
  10   9   8   7   6
@@ -7,20 +8,34 @@ package com.hanbit.matrix;
  21  22  23  24  25
  * */
 public class Zigzag {
-    public static void main(String[] args) {
-        int length = 5; // 배열 길이
-        int[][] arr = new int[length][length];
-        int k = 1; // 넣을 값
-        int flag = 1; // 스위치변수
-        int i = 0; // 행
-        int j = 0; // 열
-        
-        for(i=0; i<arr.length; i++){
-        	System.out.println("");
-        	for(j=0; j<arr[i].length; j++){
-        		System.out.printf("[%d,%d]", i,j);
-        	}
-        }
+	public static void main(String[] args) {
+	//	int length = 5; // 배열 길이
+	//	int[][] arr = new int[length][length];
+		int k = 0; // 넣을 값
+		int flag = 1; // 스위치변수
+		int i = 0; // 행
+		int j = 0; // 열
+		int[][] mtx = new int[5][5];
 
-    }
+		for (i = 0; i < 5; i++) {
+			System.out.printf("%s", "");
+			if (i % 2 == 0) {
+				for (j = 0; j < 5; j++) {
+					k++;
+					mtx[i][j]=k;
+				}
+			} else {
+				for (j = 4; j >= 0; j--) {
+					k++;
+					mtx[i][j]=k;
+				}
+			}
+		}
+		for(i=0; i<5; i++){
+			System.out.println("");
+			for(j=0;j<5;j++){
+				System.out.printf("%d", mtx[i][j]);
+			}
+		}
+	}
 }
